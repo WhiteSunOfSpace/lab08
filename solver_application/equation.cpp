5e74fab1ccf0145b308b752f39330e9ec4b27636
WhiteSunOfSpace <113097854+WhiteSunOfSpace@users.noreply.github.com>
2024-05-23 10:43:21 +0300

    Update equation.cpp

--- solver_application/equation.cpp
+++ solver_application/equation.cpp
@@ -13,19 +13,19 @@ int main()
 
     float x1 = 0;
     float x2 = 0;
-    std::ofstream out;
-    out.open("/home/logs/log.txt");
+    // std::ofstream out;
+    // out.open("/home/logs/log.txt");
 
     try
     {
         solve(a, b, c, x1, x2);
 
-        formatter(out, "x1 = " + std::to_string(x1));
-        formatter(out, "x2 = " + std::to_string(x2));
+        formatter(cout, "x1 = " + std::to_string(x1));
+        formatter(cout, "x2 = " + std::to_string(x2));
     }
     catch (const std::logic_error& ex)
     {
-        formatter(out, ex.what());
+        formatter(cout, ex.what());
     }
 
     return 0;
